c38d77012cabdef6f10ee24ca1fc863ebea81a40
StefanoDalMas <stefano.dalmas01@gmail.com>
2023-05-20 12:16:08 +0200

    Fixed spaces in clang formatter

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -6,31 +6,31 @@ local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
-  formatting.prettier,
-  formatting.stylua.with { extra_args = { "--config-path", vim.fn.expand "~/.config/nvim/.stylua.toml" } },
-  formatting.black,
-  formatting.clang_format.with {
-    extra_args = {
-      "-style={IndentWidth: 4, UseTab: Never}",
-      "-assume-filename",
-      vim.api.nvim_buf_get_name(0),
+    formatting.prettier,
+    formatting.stylua.with { extra_args = { "--config-path", vim.fn.expand "~/.config/nvim/.stylua.toml" } },
+    formatting.black,
+    formatting.clang_format.with {
+        extra_args = {
+            "-style={IndentWidth: 4, UseTab: Never, SpacesBeforeTrailingComments : 2}",
+            "-assume-filename",
+            vim.api.nvim_buf_get_name(0),
+        },
     },
-  },
-  formatting.djlint,
-  formatting.beautysh,
+    formatting.djlint,
+    formatting.beautysh,
 
-  lint.shellcheck,
-  lint.djlint,
-  lint.cpplint,
-  lint.pylint.with { extra_args = { "--errors-only" } },
-  lint.vale,
+    lint.shellcheck,
+    lint.djlint,
+    lint.cpplint,
+    lint.pylint.with { extra_args = { "--errors-only" } },
+    lint.vale,
 
-  code_actions.proselint,
+    code_actions.proselint,
 
-  diagnostics.proselint,
+    diagnostics.proselint,
 }
 
 null_ls.setup {
-  debug = true,
-  sources = sources,
+    debug = true,
+    sources = sources,
 }
